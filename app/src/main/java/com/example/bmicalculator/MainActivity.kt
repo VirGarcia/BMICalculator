28d6b5afd1ffaee44810f9476cd6b13a4bbe7516
VirGarcia <vir_garcia_minero@hotmail.com>
2024-06-04 14:03:23 +0200

    Primeros pasos en Android

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var welcomeTextView : TextView
+    lateinit var clickButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        welcomeTextView = findViewById(R.id.welcomeTextView)
+        clickButton = findViewById(R.id.clickButton)
+
+        welcomeTextView.text = getString(R.string.main_activity_welcome_text)
+
+        clickButton.setOnClickListener {
+            welcomeTextView.text = getString(R.string.main_activity_click_button)
+
+
+        }
+
     }
 }
